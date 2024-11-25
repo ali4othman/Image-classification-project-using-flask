6f5f841b5f456ebd409aa72ffb7fd94f7583a21c
Ali <ay29029@gmail.com>
2024-11-25 23:26:37 +0200

    Uploaded files!

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+<<<<<<< HEAD
+FROM python:3.10.11
+
+WORKDIR /app
+
+COPY requirements.txt .
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+EXPOSE 5000
+
+CMD ["python","app.py"]
+=======
+version https://git-lfs.github.com/spec/v1
+oid sha256:a4b1de9cab3a94200f0eb8eb4bf3c78b3e948de03688b7ca17ec292f732f481b
+size 170
+>>>>>>> origin/master
---
+++ static/script.js
@@ -0,0 +1,17 @@
+<<<<<<< HEAD
+const element = document.querySelector(".flash-message");
+const button = document.querySelector(".close-button");
+
+if (element){
+    button.onclick = function(){
+        element.remove();
+    };
+    setTimeout(() => {
+        element.remove();        
+    }, 20000);
+};
+=======
+version https://git-lfs.github.com/spec/v1
+oid sha256:a947c9e1288e964aba58ad5e437687528d8ced93cea23423c9532dd2c0e95256
+size 279
+>>>>>>> origin/master
